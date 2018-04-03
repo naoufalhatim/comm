f401cbd201965cce5c397989de97c5d7322c4182
naoufal <naoufal.hatim@usmba.ac.ma>
2018-04-03 06:31:25 +0200

    Update post.php

--- recherche/post.php
+++ recherche/post.php
@@ -2,16 +2,13 @@
 	$bdd = new PDO('mysql:host=localhost;dbname=investis04;charset=utf8','root','');
     if(isset($_POST['search']) && !empty($_POST['search'])){
 		$search = $_POST['search'];
-		
-
-	
-		$sql=("SELECT * FROM quartier WHERE  nom_quartier LIKE '".$search."%' and id_type=2 ORDER BY  nom_quartier DESC");
-		
-	
+		if(isset($_COOKIE['q']) && !empty($_COOKIE['q'])){
+		$q = $_COOKIE['q'];
+		 }
+		$sql=("SELECT * FROM quartier WHERE  nom_quartier LIKE '".$search."%' and id_type='".$q."' ORDER BY  nom_quartier DESC");
 				$rep = $bdd->query($sql); 
         while ($rows = $rep ->fetch()){					   
 			print('<li><a href="../results.php?id='.$rows["code_quartier"].'&nom_quartier='.$rows["nom_quartier"].'&type_quartier='.$rows["id_type"].'">'.$rows["nom_quartier"].'</a></li>');
 		}
 	}
-
 ?>
